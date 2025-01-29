c18ead997a7d6bb8d3c9e5241fb1b2520deafd87
Renata Mota <renatamota0814@gmail.com>
2025-01-28 23:45:08 -0300

    add Page Objects

--- cypress/pages/menuPage.js
+++ cypress/pages/menuPage.js
@@ -1,12 +1,19 @@
 class menuPage {
     selectorList () {
         const selectors ={
-            myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]'
+            myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+            performanceButton: '[href="/web/index.php/performance/viewPerformanceModule"]'
         }  
+
         return selectors
     }
+
     accessMyInfo() {
         cy.get(this.selectorList().myInfoButton).click()
+    }
+
+    accessPerformance(){
+        cy.get(this.selectorList().performanceButton).click()
     }
 }
 export default menuPage
